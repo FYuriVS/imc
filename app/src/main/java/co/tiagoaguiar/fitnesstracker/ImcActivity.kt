8371ccd6c42ce61430cb3231eaa5fc35c3df2dff
Yuri <fyurivs@outlook.com>
2023-07-19 17:51:24 -0300

    Criação do arquivo style

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -2,10 +2,48 @@ package co.tiagoaguiar.fitnesstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import co.tiagoaguiar.fitnesstracker.databinding.ActivityImcBinding
+import co.tiagoaguiar.fitnesstracker.databinding.ActivityMainBinding
 
 class ImcActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityImcBinding
+
+    private lateinit var weight: EditText
+    private lateinit var height: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_imc)
+        binding = ActivityImcBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        weight = binding.editImcWeight
+        height = binding.editImcHeight
+        val btnSend = binding.btnImc
+
+        var res: Double
+
+        btnSend.setOnClickListener() {
+            res = calcImc(weight.text.toString().toInt(), height.text.toString().toInt())
+            if(!validateForm()){
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else {
+                Toast.makeText(this, "Seu IMC é: $res", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun calcImc(w: Int, h: Int): Double {
+        return w / ((h/100.0) * (h/100.0))
+    }
+
+    private fun validateForm(): Boolean {
+        return (weight.text.toString().isNotEmpty()
+            && height.text.toString().isNotEmpty()
+            && !weight.text.toString().startsWith("0")
+            && !height.text.toString().startsWith("0")
+        )
     }
 }
